8792a15ffe4734f848922be4f2db3b95f7a70a2a
Rangel Ivanov <radito3@mail.bg>
2019-04-21 18:52:26 +0300

    Update tools - Add moving drones by area needs

--- dronathone/reader.py
+++ dronathone/reader.py
@@ -8,13 +8,15 @@ def read_drones_from_json():
             drones.append(json.loads(drone[0]))
     return drones
 
+
 def read_parcels_from_json():
     parcels = []
-    for i in range(1,6):
+    for i in range(1, 6):
         with open('data/parcels/parcel' + str(i) + '.json') as json_file:
             for parcel in json.load(json_file):
                 parcels.append(json.loads(parcel[0]))
     return parcels
+
 
 def read_stations_from_json():
     stations = []
@@ -22,3 +24,4 @@ def read_stations_from_json():
         for station in json.load(json_file):
             stations.append(json.loads(station[0]))
     return stations
+

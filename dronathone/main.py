d0c3c28f0d455e8e0c74cfd85e4e1a0273dd5a86
iliancho99 <iliqn.kordev@gmail.com>
2019-04-21 17:45:01 +0300

    Add parcel Percentage calculator

--- dronathone/main.py
+++ dronathone/main.py
@@ -2,18 +2,36 @@ import reader as r
 import tools as t
 import sys
 import json
+import math
 
 drones = r.read_drones_from_json()
 station = r.read_stations_from_json()
 parcels = r.read_parcels_from_json()
 
 drone_delivery = {}
-drone_count = 0
-for drone in drones:
+total_drones = len(drones)
+total_parsels = 0
+for i in range(0, len(parcels)):
+    total_parsels += len(parcels[i])
 
-    drone_delivery[drone_count] = sys.maxsize
+parcels_percentage_of_all = []
 
-    for parcel in parcels:
+for i in range(0, len(parcels)):
+    parcels_percentage_of_all.append((len(parcels[i])/ total_parsels) * 100)
 
-        dist = t.distance(parcel['location'], drone['location'])
-        print(dist)
+print(parcels_percentage_of_all)
+drones_per_area = []
+
+for i in range(len(parcels_percentage_of_all)):
+    drones_per_area.append(int(round(parcels_percentage_of_all[i] * total_drones)/100))
+
+
+print(drones_per_area)
+# for drone in drones:
+#
+#     drone_delivery[drone_count] = sys.maxsize
+#
+#     for parcel in parcels:
+#
+#         dist = t.distance(parcel['location'], drone['location'])
+#         print(dist)
--- dronathone/reader.py
+++ dronathone/reader.py
@@ -10,10 +10,12 @@ def read_drones_from_json():
 
 def read_parcels_from_json():
     parcels = []
-    for i in range(1,6):
-        with open('data/parcels/parcel' + str(i) + '.json') as json_file:
-            for parcel in json.load(json_file):
-                parcels.append(json.loads(parcel[0]))
+    for i in range(0,7):
+        parcels_in_area = []
+        with open('data/parcels/parcel' + str(i)+'.json') as json_file:
+            for parcel_in_area in json.load(json_file):
+                parcels_in_area.append(json.loads(parcel_in_area[0]))
+        parcels.append(parcels_in_area)
     return parcels
 
 def read_stations_from_json():
@@ -22,3 +24,16 @@ def read_stations_from_json():
         for station in json.load(json_file):
             stations.append(json.loads(station[0]))
     return stations
+
+def read_warehouses_from_json():
+    warehouses = [
+    ["NETWORKING: Premium Coworking Space [Dronathon] {\"id\":0, \"location\": [42.7058171,23.3287061]}",42.7058171,23.3287061],
+    ["Business Center Serdika {\"id\":1, \"location\": [42.691163,23.294588]}",42.691163,23.294588],
+    ["Bulgaria Mall {\"id\":2, \"location\": [42.6643476,23.289005]}",42.6643476,23.289005],
+    ["Airport Sofia [Terminal 1] {\"id\":3, \"location\": [42.6895872,23.4027335000001]}",42.6895872,23.4027335000001],
+    ["SoftUni {\"id\":4, \"location\": [42.6666605,23.3506835000001]}",42.6666605,23.3506835000001],
+    ["NBU {\"id\":5, \"location\": [42.67786, 23.25269]}",42.67786, 23.25269],
+    ["Lyulin, Sofia {\"id\":6, \"location\": [42.723894,23.2375028]}",42.723894,23.2375028],
+]
+
+    return warehouses;
